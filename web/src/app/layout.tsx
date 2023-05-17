fef8aa8bebb2e04452461081c6141c58f5098cdc
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-05-16 21:31:23 -0300

    feat: add fonts

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+
+const baiJanjuree = BaiJamjuree({
+  subsets: ['latin'],
+  weight: '700',
+  variable: '--font-bai-janjuree',
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -16,7 +25,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${roboto.variable} ${baiJanjuree.variable} font-sans`}>
+        {children}
+      </body>
     </html>
   )
 }

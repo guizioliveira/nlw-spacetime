eac2d864d0d25ff917d4ac73f44e34fdcb126aa0
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2023-05-17 23:33:53 -0300

    refactor: refactoring frontend elements on componentes

---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Made with 💜 in Rocketseat&apos;s NLW by{' '}
+      <a
+        className="underline transition-colors hover:text-gray-100"
+        href="https://github.com/guizioliveira"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Guilherme de Oliveira
+      </a>
+    </div>
+  )
+}
---
+++ web/src/components/index.ts
@@ -0,0 +1,4 @@
+export * from './Copyright'
+export * from './Hero'
+export * from './SignIn'
+export * from './EmptyMemories'
